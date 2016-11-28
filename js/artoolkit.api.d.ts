2fe6f109b24d56c68927c04eaed9abbdfaa2fa75
Hakan Dilek <hakandilek@gmail.com>
2016-11-28 17:30:57 +0100

    type defs for artoolkit.api

---
+++ js/artoolkit.api.d.ts
@@ -0,0 +1,19 @@
+﻿// Type definitions for Javascript ARToolKit v5.x 
+// Project: https://github.com/artoolkit/jsartoolkit5
+// Definitions by: Hakan Dilek <https://github.com/hakandilek>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped  
+
+export declare class ARController {
+    width: number;
+    height: number;
+    camera: ARCameraParam;
+    constructor(width: number, height: number, cameraData: string | ARCameraParam);
+
+    onload(): void;
+    debugSetup(): void;
+    process(image: any): void;
+}
+
+export declare interface ARCameraParam {
+
+}
