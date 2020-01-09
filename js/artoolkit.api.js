a4faed2ef35e0cd6fd686fafb04ef49e4c190ac6
kalwalt <github@kalwaltart.it>
2020-01-09 19:05:05 +0100

    setLogLevel, GetLogLevel, setMarkerInfoDir docs

--- js/artoolkit.api.js
+++ js/artoolkit.api.js
@@ -952,16 +952,26 @@
 	/**
 		Sets the logging level to use by ARToolKit.
 
-		@param
+		@param {number} mode type for the log level.
 	*/
     ARController.prototype.setLogLevel = function (mode) {
         return artoolkit.setLogLevel(mode);
     };
-
+  /**
+  	Gets the logging level used by ARToolKit.
+  */
     ARController.prototype.getLogLevel = function () {
         return artoolkit.getLogLevel();
     };
-
+  /**
+    Sets the dir (direction) of the marker. Direction that tells about the rotation
+    about the marker (possible values are 0, 1, 2 or 3).
+    This parameter makes it possible to tell about the line order of the detected marker
+     (so which line is the first one) and so find the first vertex.
+    This is important to compute the transformation matrix in arGetTransMat().
+    @param {number} markerIndex the index of the marker
+    @param {number} dir direction of the marker (possible values are 0, 1, 2 or 3).
+  */
     ARController.prototype.setMarkerInfoDir = function (markerIndex, dir) {
         return artoolkit.setMarkerInfoDir(this.id, markerIndex, dir);
     };
